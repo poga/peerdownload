7cee62a6fdf712fe019ecf0b8a4ac9a8e96ceae2
Poga Po <poga.bahamut@gmail.com>
2016-07-10 20:03:27 +0800

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# peerdownload
+
+Convert a normal download link into a P2P file distribution network. Save bandwidth.
+
+## Synopsis
+
+```html
+<script src="peerdownload.js"></script>
+
+<a href="A_HUGE_FILE.dat" peerdownload>Download</a>
+```
+
+just add `peerdownload` attribute to the link you want to convert.
+
+## license
+
+MIT
---
+++ index.js
@@ -0,0 +1,90 @@
+const hyperdrive = require('hyperdrive')
+const memdb = require('memdb')
+const http = require('stream-http')
+const crypto = require('crypto')
+const swarm = require('hyperdrive-archive-swarm')
+const signalhub = require('signalhub')
+const normalize = require('normalize-url')
+const toBlobURL = require('stream-to-blob-url')
+const path = require('path')
+
+var hub = signalhub('peerdownload', ['https://signalhub.mafintosh.com'])
+var drive = hyperdrive(memdb())
+
+var fileHashes = {}
+
+var channel = signature(window.location.href)
+var sub = hub.subscribe(channel)
+sub.on('data', (msg) => {
+  if (msg.type === 'fetch') {
+    if (Object.keys(fileHashes).length > 0) {
+      hub.broadcast(channel, {type: 'hash', hashes: fileHashes})
+    }
+  } else if (msg.type === 'hash') {
+    Object.keys(msg.hashes).forEach((url) => {
+      fileHashes[url] = msg.hashes[url]
+    })
+  }
+})
+hub.broadcast(channel, {type: 'fetch'})
+
+document.addEventListener('DOMContentLoaded', hook)
+
+function hook () {
+  document.querySelectorAll('[peerdownload]').forEach((dom) => {
+    dom.addEventListener('click', (e) => {
+      e.preventDefault()
+
+      download(dom.href)
+    })
+  })
+}
+
+function download (fileURL) {
+  console.log(fileURL)
+  if (fileHashes[signature(fileURL)]) {
+    // if it's a known file in fileHashes, use peerdownload
+    var archive = drive.createArchive(fileHashes[signature(fileURL)])
+    swarm(archive)
+    downloadArchiveFile(archive, fileURL, () => { console.log('done') })
+  } else {
+    var archive = drive.createArchive()
+    var ws = archive.createFileWriteStream('data')
+    http.get(fileURL, (res) => {
+      res.pipe(ws).on('finish', () => {
+        archive.finalize(() => {
+          swarm(archive)
+          downloadArchiveFile(archive, fileURL, () => {
+            fileHashes[signature(fileURL)] = archive.key.toString('hex')
+            hub.broadcast(channel, {type: 'hash', hashes: fileHashes})
+          })
+        })
+      })
+    })
+  }
+}
+
+function signature (url) {
+  return crypto.createHash('sha256').update(normalize(url)).digest('hex')
+}
+
+function saveBlobURL (fileURL, blobURL) {
+  var a = document.createElement('a')
+  document.body.appendChild(a)
+  a.style = "display: none"
+  a.href = blobURL
+  a.download = path.basename(fileURL)
+  a.click()
+}
+
+function downloadArchiveFile(archive, fileURL, cb) {
+  var rs = archive.createFileReadStream('data')
+  toBlobURL(rs, (err, blobURL) => {
+    if (err) {
+      return cb(err)
+    }
+
+    saveBlobURL(fileURL, blobURL)
+    cb()
+  })
+}
